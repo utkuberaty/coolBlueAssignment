354a9776d99caa2fc8d212a6f4af37c5b5a61424
Utku Yıldız <yildiz.utku97@gmail.com>
2022-02-08 20:28:28 +0300

    initial commit

---
+++ app/src/main/java/com/utku/coolblueassignment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.utku.coolblueassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
